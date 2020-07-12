37446b792bf8b12c93fcbfedff39eab9e4b41427
Mohit Gupta <59468087+mohitg1998@users.noreply.github.com>
2020-07-12 14:39:57 +0530

    Update camera.py

--- Facial Expression Recognition with Keras/camera.py
+++ Facial Expression Recognition with Keras/camera.py
@@ -12,12 +12,12 @@ class VideoCamera(object):
         self.video = cv2.VideoCapture(0)       #For facial expression from webcome ,0 set to the source
 
     def __del__(self):
-        self.video.release()
+        self.video.release()                
 
     # returns camera frames along with bounding boxes and predictions
     def get_frame(self):
-        _, fr = self.video.read()
-        gray_fr = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
+        _, fr = self.video.read()            #Read the frames from the video as numpy array            check,frames
+        gray_fr = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)                  #Convert image into grayscale image
         faces = facec.detectMultiScale(gray_fr, 1.3, 5)
 
         for (x, y, w, h) in faces:
